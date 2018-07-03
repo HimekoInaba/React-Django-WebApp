abcfc35dd495b8bd7cb0d48fb1176170e1ed8981
Syrym <s.tusupbekov@p-s.kz>
2018-07-03 23:48:37 +0600

    First commit

---
+++ frontend/src/LoginPage.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./LoginForm.css"
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      login: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    this.setState(() => {
+      return {
+        login: this.login.value,
+        email: this.email.value,
+        password: this.password.value,
+        confirmPassword: this.confirmPassword.value
+      };
+    });
+
+    console.log(this.state.login);
+
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="loginPageMain">
+        <div className="form">
+          <form onSubmit={this.authorize}>
+            <input type="text" id="login" name="login"
+                   placeholder="Login"
+                    ref={(a) => this.login = a}/><br/>
+            <input type="email" id="email" name="email"
+                   placeholder="Email"
+                   ref={(a) => this.email = a}/><br/>
+            <input type="password" id="password" name="password"
+                   placeholder="Password"
+                   ref={(a) => this.password = a}/><br/>
+            <input type="password" id="confirmPassword" name="confirmPassword"
+                   placeholder="Confirm password"
+                   ref={(a) => this.confirmPassword = a}/><br/>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default LoginPage;
