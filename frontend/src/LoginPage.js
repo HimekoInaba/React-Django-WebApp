7d2ee42e026cd25ca109eb4be54b25e9e380457a
HimekoInaba <s.tusupbekov@p-s.kz>
2018-07-05 18:12:23 +0600

    Login page reworked

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -30,6 +30,19 @@ class LoginPage extends Component {
     e.preventDefault();
   }
 
+  send(credentials) {
+    fetch('https://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      credentials
+    })
+})
+  }
+
   render() {
     return (
       <div className="loginPageMain">
